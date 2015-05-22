f369abf3f25fa8e640e7dab98449b8f065269490
Drullkus <ericsplanet@mac.com>
2015-05-22 14:05:51 -0700

    Hurr durr

--- src/main/java/com/drullkus/StuffsAndThings/Renderer/RendererTESRTest.java
+++ src/main/java/com/drullkus/StuffsAndThings/Renderer/RendererTESRTest.java
@@ -1,6 +1,7 @@
 package com.drullkus.StuffsAndThings.Renderer;
 
 import com.drullkus.StuffsAndThings.Blocks.TileBlockTest;
+import com.drullkus.StuffsAndThings.StuffsAndThings;
 import com.drullkus.StuffsAndThings.Tile.TETest;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -11,7 +12,7 @@ import org.lwjgl.opengl.GL11;
 public class RendererTESRTest extends TileEntitySpecialRenderer {
 
     public PortalGenerator modelObj;
-    public final ResourceLocation modelTexture = new ResourceLocation("tabula", "textures/model/tabulaRasa.png");
+    public final ResourceLocation modelTexture = new ResourceLocation(StuffsAndThings.MOD_ID, "textures/model/portalGenerator.png");
 
     public RendererTESRTest()
     {
@@ -21,9 +22,13 @@ public class RendererTESRTest extends TileEntitySpecialRenderer {
 
     public void renderTileEntityAt(TETest tileEntity, double x, double y, double z, float partialTick)
     {
+        GL11.glPushMatrix();
+
         this.bindTexture(modelTexture);
         GL11.glTranslated(x, y, z);
         this.modelObj.render(0.0625F);
+
+        GL11.glPopMatrix();
     }
 
     @Override
